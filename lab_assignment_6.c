2d04544c38f4248208f1a6d460330e7598dd4d8a
Elldarm1 <emizrachi1@knights.ucf.edu>
2023-06-23 17:53:18 -0400

    adding lab 6 file

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,6 +2,19 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
+	int mid;
+    if(low <= high){
+        mid = (low + high) / 2;
+        if(value < numbers[mid]){
+            return search(numbers, low, mid - 1, value);
+        }
+        if(value > numbers[mid]){
+            return search(numbers, mid + 1, high, value);
+        }
+        else{
+            return mid;
+        }
+    }
 	return -1;
 }
 
